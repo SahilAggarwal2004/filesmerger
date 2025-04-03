2123be4e64673ea11476bba0d8d8c66c5fe4e2c8
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2025-04-03 23:20:51 +0530

    [feat]: PWA

---
+++ next.config.mjs
@@ -0,0 +1,42 @@
+import fs from "fs"
+import withSerwistInit from "@serwist/next";
+import packageJSON from "./package.json" with { type: "json" };
+
+const pages = ["/", "/image-merger"];
+const revision = Date.now().toString();
+
+const withPWA = withSerwistInit({
+  swSrc: "src/sw.ts",
+  swDest: "public/sw.js",
+  exclude: [/public\/sw.js/],
+  disable: process.env.NODE_ENV === "development",
+  register: false,
+  reloadOnOnline: false,
+  additionalPrecacheEntries: pages.map((url) => ({ url, revision })),
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    nextScriptWorkers: true,
+  },
+};
+
+const manifestPath = "./public/manifest.json";
+
+const updateManifestVersion = () => {
+  if (fs.existsSync(manifestPath)) {
+    const manifest = JSON.parse(fs.readFileSync(manifestPath, "utf8"));
+    manifest.version = packageJSON.version;
+    manifest.id = packageJSON.version;
+    fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2) + "\n");
+    console.log(`✅ Manifest version updated to ${packageJSON.version}`);
+  } else {
+    console.warn("⚠️  manifest.json not found!");
+  }
+};
+
+updateManifestVersion();
+
+export default withPWA(nextConfig);
